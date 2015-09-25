b8533eb95f7fa76afa7f2fb59a3c3dde0de267b6
raluca-elena <ralucaelena1985@gmail.com>
2015-09-25 12:38:08 +0300

    transparent overlay actionbar

--- app/src/main/java/com/example/android/momintuition/Anim.java
+++ app/src/main/java/com/example/android/momintuition/Anim.java
@@ -15,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
@@ -47,10 +48,8 @@ public class Anim extends ActionBarActivity implements GoogleApiClient.Connectio
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page_animation);
-        //findViewById(R.id.action_settings).getBackground().setAlpha(0);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        //actionBar.setStackedBackgroundDrawable(new ColorDrawable(Color.parseColor("#550000ff")));;
 
 
         ViewGroup v = (ViewGroup) findViewById(R.id.abc);
