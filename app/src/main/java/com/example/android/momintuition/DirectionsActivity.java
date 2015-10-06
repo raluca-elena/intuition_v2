399cbfaa30efc687d584caedcb693bb3a5ad2431
raluca-elena <ralucaelena1985@gmail.com>
2015-10-06 17:56:25 +0300

    switch again to MapView to investigate issue on stack overflow

--- app/src/main/java/com/example/android/momintuition/DirectionsActivity.java
+++ app/src/main/java/com/example/android/momintuition/DirectionsActivity.java
@@ -1,18 +1,17 @@
 package com.example.android.momintuition;
-
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -20,24 +19,53 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class DirectionsActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-
-    GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
+    LinearLayout linearLayout;
+    MapView mapView;
+    GoogleMap map;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#40000000")));
+
+
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng mapCenter = new LatLng(41.889, -87.622);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, 13));
+        this.map = googleMap;
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        map.animateCamera(cameraUpdate);
+    }
 
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        Log.i("AHA", "why onresume happens now?");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
