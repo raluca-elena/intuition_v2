92cdb00cb44cc329731e942361e1d78f8f2e8842
raluca-elena <ralucaelena1985@gmail.com>
2015-10-07 16:15:19 +0300

    draw path from dirrections google api

--- app/src/main/java/com/example/android/momintuition/DirectionsActivity.java
+++ app/src/main/java/com/example/android/momintuition/DirectionsActivity.java
@@ -1,13 +1,23 @@
 package com.example.android.momintuition;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,9 +27,23 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class DirectionsActivity extends AppCompatActivity implements OnMapReadyCallback {
-    LinearLayout linearLayout;
     MapView mapView;
     GoogleMap map;
     @Override
@@ -32,15 +56,74 @@ public class DirectionsActivity extends AppCompatActivity implements OnMapReadyC
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#40000000")));
 
-
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.map = googleMap;
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.65331,-79.38277), 5);
         map.animateCamera(cameraUpdate);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyDISYkoIYzQesb1VQ0eQQ6x0BaWxD87xWg";
+        //url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&key=AIzaSyBbE2wO2MDZ2goETgsY__ifEq2dlOMLLc4";
+        JsonObjectRequest stringRequest = new JsonObjectRequest(JsonObjectRequest.Method.GET, url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        drawPath(response);
+                    }} , new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.i("this is the error", error + "");}
+                     });
+        int  MY_SOCKET_TIMEOUT_MS = 9000;
+        Log.i("max retries", DefaultRetryPolicy.DEFAULT_MAX_RETRIES + "");
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                MY_SOCKET_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+
+    public void drawPath(JSONObject response){
+        JSONArray routeArray = null;
+        try {
+            routeArray = response.getJSONArray("routes");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONObject routes = null;
+        try {
+            routes = routeArray.getJSONObject(0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONObject overviewPolylines = null;
+        try {
+            overviewPolylines = routes.getJSONObject("overview_polyline");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String encodedString = null;
+        try {
+            encodedString = overviewPolylines.getString("points");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        List<LatLng> list = decodePoly(encodedString);
+
+        Polyline line = map.addPolyline(new PolylineOptions()
+                        .addAll(list)
+                        .width(12)
+                        .color(Color.parseColor("#05b1fb"))//Google maps blue color
+                        .geodesic(true)
+        );
+
+
     }
 
 
@@ -68,4 +151,42 @@ public class DirectionsActivity extends AppCompatActivity implements OnMapReadyC
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
+
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+
+
 }
