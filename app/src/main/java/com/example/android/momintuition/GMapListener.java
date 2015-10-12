5420fdc3f3b24d0a86557c22f11ea6eb1ff8afc4
raluca-elena <ralucaelena1985@gmail.com>
2015-10-12 16:40:47 +0300

    working notification on maps and talk

--- app/src/main/java/com/example/android/momintuition/GMapListener.java
+++ app/src/main/java/com/example/android/momintuition/GMapListener.java
@@ -5,16 +5,10 @@ package com.example.android.momintuition;
  */
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.Notification;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.os.Parcelable;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
 
 
 public class GMapListener extends AccessibilityService{
@@ -23,14 +17,10 @@ public class GMapListener extends AccessibilityService{
 
         @Override
         public void onCreate() {
-//         getServiceInfo().flags = AccessibilityServiceInfo.FLAG_REQUEST_TOUCH_EXPLORATION_MODE;
            Log.i("on create ser", "SERVICE");
         }
 
-
-
-
-
+    //helper function not used yet
     private String getEventType(AccessibilityEvent event) {
         switch (event.getEventType()) {
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
@@ -62,37 +52,19 @@ public class GMapListener extends AccessibilityService{
     }
 
 
-
-
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.i("GOT AN EVENT", event+"");
-        Log.i("EVENT", String.format(
-                "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [text] %s",
-                getEventType(event), event.getClassName(), event.getPackageName(),
-                event.getEventTime(), getEventText(event)));
-
 
         if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
-            Log.d("TORTUGA","Recieved event");
             Parcelable data = event.getParcelableData();
             if (data instanceof Notification) {
-                Log.i("Tortuga","Recieved notification");
                 Notification notification = (Notification) data;
-                Log.i("Tortuga","ticker: " + notification.tickerText);
-                Log.i("Tortuga","icon: " + notification.icon);
-                Log.i("Tortuga", "notification: "+ event.getText());
+                Log.i("NOTIFICATION","ticker: " + notification.tickerText);
+                Log.i("NOTIFICATION","icon: " + notification.icon);
+                Log.i("NOTIFICATION", "notification getText: "+ event.getText());
+                Log.i("NOTIFICATION", "verbose"+ getEventText(event));
             }
 
-            if (getEventType(event) == "default"){
-                Log.i("IACA", "EVRICA");
-                Parcelable data1 = event.getParcelableData();
-                Log.i("DATA1", data1+"");
-
-
-
-
-            }
         }
     }
 
@@ -100,96 +72,6 @@ public class GMapListener extends AccessibilityService{
     public void onInterrupt() {
         Log.v(TAG, "onInterrupt");
     }
-
-    /*@Override
-    protected void onServiceConnected() {
-        super.onServiceConnected();
-        Log.i("TORTUGA", "onServiceConnected" + " " + getServiceInfo());
-
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        //info.flags = AccessibilityServiceInfo.DEFAULT;
-        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-        info.feedbackType = AccessibilityServiceInfo.DEFAULT;
-
-
-        //info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
-        //info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
-
-        //info.packageNames = new String[]
-        //        { "com.google.android.talk", "com.google.android.apps.maps", };
-
-        //info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
-
-        //info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
-        setServiceInfo(info);
-    }*/
-/*
-    @Override
-        public void onAccessibilityEvent(AccessibilityEvent event) {
-            Log.i("EVENT", event.toString());
-            //AccessibilityNodeInfo nodeInfo = event.getSource();
-            //nodeInfo.performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
-            //nodeInfo.recycle();
-
-            final int eventType = event.getEventType();
-            String eventText = null;
-            switch(eventType) {
-                case AccessibilityEvent.TYPE_VIEW_CLICKED:
-                    eventText = "Focused: ";
-                    break;
-                case AccessibilityEvent.TYPE_VIEW_FOCUSED:
-                    eventText = "Focused: ";
-                    break;
-            }
-
-            eventText = eventText + event.getContentDescription();
-
-            // Do something nifty with this text, like speak the composed string
-            // back to the user.
-            //speakToUser(eventText);
-
-            Log.i("SPeak", eventText + "");
-
-
-        }
-
-        @Override
-        public void onInterrupt() {
-            Log.i("onInterr", "user did something");
-        }
-
-
-
-    @Override
-    protected void onServiceConnected()
-    {
-        Log.i("AAAAAAAAAAA", "ServiceConnected");
-        try
-        {
-            AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-
-
-            info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED |
-                    AccessibilityEvent.TYPE_VIEW_FOCUSED;
-
-            info.packageNames = new String[]
-                    { "com.google.android.talk", "com.google.android.apps.maps", };
-
-            Log.i("my info", info.toString() + "");
-
-            info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
-
-            info.notificationTimeout = 100;
-
-            setServiceInfo(info);
-            this.setServiceInfo(info);
-        }
-        catch(Exception e)
-        {
-            Log.d("ERROR Connected", e.toString());
-        }
-    }*/
-
 
 
 }
